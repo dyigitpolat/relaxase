fdc76d3b399e5780dd1903049f79c6fe6d825816
ayanga kalupahana <ayangakalupahana@gmail.com>
2020-11-22 23:26:24 -0800

    schifra added

---
+++ CMakeFiles/reed-solomon-master.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reed-solomon-master"
+  "CMakeFiles/reed-solomon-master-complete"
+  "reed-solomon-master-prefix/src/reed-solomon-master-stamp/reed-solomon-master-build"
+  "reed-solomon-master-prefix/src/reed-solomon-master-stamp/reed-solomon-master-configure"
+  "reed-solomon-master-prefix/src/reed-solomon-master-stamp/reed-solomon-master-download"
+  "reed-solomon-master-prefix/src/reed-solomon-master-stamp/reed-solomon-master-install"
+  "reed-solomon-master-prefix/src/reed-solomon-master-stamp/reed-solomon-master-mkdir"
+  "reed-solomon-master-prefix/src/reed-solomon-master-stamp/reed-solomon-master-patch"
+  "reed-solomon-master-prefix/src/reed-solomon-master-stamp/reed-solomon-master-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/reed-solomon-master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
