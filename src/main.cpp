cf2fc491a2b0a02f7c43047516f221a90ff5e71f
yigit polat <yigit@u.nus.edu>
2020-11-24 23:14:57 +0800

    dna coder

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include "relaxase.hpp"
 #include "relaxase_client.hpp"
-#include "dna_encoder.hpp"
-//#include "dna_decoder.hpp"
+#include "dna_coder.hpp"
 
 int main()
 {
     std::cout << "Welcome to relaxase!!" << std::endl;
     RelaxaseClient r;
     uint8_t input[1000000];
-    uint8_t *in = &input[0];
+    uint8_t *in= &input[0];
     uint8_t output[1000000];
-    uint8_t *ou = &output[0];
-    int count = r.add_new_file("example", in);
-
+    uint8_t *ou= &output[0];
+    int count=r.add_new_file("example",in);
+   
     //r.retrieve_file("example",in);
     std::cout << "Finished encryption" << std::endl;
-    DNAEncoder encoder;
-    encoder.encode_file("example_encrypt.dat", 10000, 0, 1, "proirity_file.txt", "output_file.txt"); //output_file.txt contains encoded DNA strands
+    DNACoder coder;
+    coder.encode_file("data/example_encrypt.dat", 10000, 0, 1 , "data/proirity_file.txt", "data/encoded_file.txt"); //output_file.txt contains encoded DNA strands
     std::cout << "Finished encoding" << std::endl;
-    //DNADecoder decoder;
-    //decoder.decode_file("output_file.txt",10000,0,1, "proirity_file.txt", "decoded_output.dat"); //decoded_output.txt contains decoded data
-    //r.retrieve_file("decoded_output",ou);
+    coder.decode_file("data/encoded_file.txt",10000,0,1, "data/proirity_file.txt", "data/decoded_file.dat"); //decoded_output.txt contains decoded data
+    std::cout << "Finished decoding" << std::endl;
+    r.retrieve_file("data/decoded_file",ou);
+    std::cout << "Finished decryption" << std::endl;
     return 0;
 }
